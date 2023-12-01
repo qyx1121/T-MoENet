8e66b2e751246dc267ea6ee8cd6da0f954627623
qinyixin <qyxzn715@gmail.com>
2023-12-01 10:19:35 +0800

    update

--- scripts/eval_oe.sh
+++ scripts/eval_oe.sh
@@ -1,4 +1,4 @@
-python inference_oe.py --dataset_path /mnt/hdd3/qinyixin/FrozenBilm/MSVD-QA/test.csv \
---feat_path /mnt/hdd3/qinyixin/FrozenBilm/MSVD-QA/clipvitl14.pth \
---vocab_path /mnt/hdd3/qinyixin/FrozenBilm/MSVD-QA/vocab1000.json \
---model_path /mnt/hdd1/qinyixin/FrozenBilm/ckpts/deberta_moe_ffn_first_2/checkpoint0004.pth \
+python inference_oe.py --dataset_path /mnt/hdd3/qinyixin/T-MoENet/MSVD-QA/test.csv \
+--feat_path /mnt/hdd3/qinyixin/T-MoENet/MSVD-QA/clipvitl14.pth \
+--vocab_path /mnt/hdd3/qinyixin/T-MoENet/MSVD-QA/vocab1000.json \
+--model_path /mnt/hdd1/qinyixin/T-MoENet/ckpts/deberta_moe_ffn_first_2/checkpoint0004.pth \
--- scripts/train_webvid.sh
+++ scripts/train_webvid.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --master_port=1121 --nproc_per_node 4 main.py \
---combine_datasets webvid --combine_datasets_val webvid --save_dir=/mnt/hdd1/qinyixin/FrozenBilm/ckpts/moe_deberta_2_expert/ \
+--combine_datasets webvid --combine_datasets_val webvid --save_dir=/mnt/hdd1/qinyixin/T-MoENet/ckpts/moe_deberta_2_expert/ \
 --lr=3e-5 --ds_factor_ff=8 --ds_factor_attn=8 --add_video_feat \
 --batch_size=32 --batch_size_val=16 --epochs=5 --webvid_features_path /mnt/hdd3/qinyixin/feats \
 --eval_skip 1 --webvid_train_csv_path data/exp_2/train_2m.csv
